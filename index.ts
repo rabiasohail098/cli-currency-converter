23e53edd6095fdbd1b95331a7de2565af5f03358
Rabia-Sohail <rabiasohail1209@gmail.com>
2024-04-07 01:25:39 +0500

    Created cli based simple currency converter

---
+++ index.ts
@@ -0,0 +1,37 @@
+
+
+import inquirer from "inquirer";
+
+const currency: any = {
+    USD:1, // 1 dollar ko sub currency me convert ker k yea value save ki hain
+    EUR:0.91,
+    GBP:0.76,
+    INR:74.57,
+    PKR:280,
+}
+let userAnswer = await inquirer.prompt(
+    [
+        {name:"from",
+        message:"Enter from Currency",
+        type:"list",
+        choices:["USD","EUR","GBP","INR","PKR"]
+     },
+     {name:"to",
+     message:"Enter to Currency",
+     type:"list",
+     choices:["USD","EUR","GBP","INR","PKR"]
+  },
+  {name:"amount",
+  message:"Enter Your Amount",
+  type:"number",
+  }
+    ]
+)
+console.log(userAnswer)
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedamount = baseAmount * toAmount
+console.log(convertedamount);
